bf0f61c042ea301983eb5306d71df4d89d14abef
white-gyu <white_gyu@naver.com>
2023-05-17 15:50:46 +0900

    chore: init project

---
+++ hjna/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/InstagramByKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.flata.instagram
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InstagramByKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<InstagramByKotlinApplication>(*args)
+}
