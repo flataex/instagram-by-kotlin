efbbbaea267f861662f4fd50448b1c5f8cede462
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2023-05-28 22:21:46 +0900

    refactor: 회원가입, 로그인 로직 리팩토링

--- shkim/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/user/service/UserService.kt
+++ shkim/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/user/service/UserService.kt
@@ -15,43 +15,41 @@ class UserService(
     private val bCryptPasswordEncoder: BCryptPasswordEncoder
 ) {
 
-    fun save(
-        request: UserSaveRequest
-    ): Long {
-        validate(request)
-
-        val user = User(
-            email = request.email,
-            password = bCryptPasswordEncoder.encode(request.password),
-            nickname = request.nickname
-        )
-
-        return userRepository.save(user).id
+    fun save(request: UserSaveRequest): Long {
+        return request.validateEmail()
+            .validateNickname()
+            .createUser()
+            .saveUser()
+            .id
     }
 
-    private fun validate(
-        request: UserSaveRequest
-    ) {
-        if (userRepository.existsByEmail(request.email)) {
-            throw RuntimeException("중복된 이메일입니다.")
-        }
+    private fun UserSaveRequest.validateEmail(): UserSaveRequest {
+        require(!userRepository.existsByEmail(email)) { "중복된 이메일입니다." }
+        return this
+    }
 
-        if (userRepository.existsByNickname(request.nickname)) {
-            throw RuntimeException("중복된 닉네임입니다.")
-        }
+    private fun UserSaveRequest.validateNickname(): UserSaveRequest {
+        require(!userRepository.existsByNickname(nickname)) { "중복된 닉네임입니다." }
+        return this
+    }
+
+    private fun UserSaveRequest.createUser(): User {
+        return User(
+            email = email,
+            password = bCryptPasswordEncoder.encode(password),
+            nickname = nickname
+        )
+    }
+
+    private fun User.saveUser(): User {
+        return userRepository.save(this)
     }
 
     fun login(
         request: UserLoginRequest
-    ): Long {
-        val user = userRepository.findByEmail(request.email)
-            ?: throw RuntimeException("존재하지 않는 계정입니다.")
-
-        if (!bCryptPasswordEncoder.matches(request.password, user.password)) {
-            throw RuntimeException("비밀번호가 일치하지 않습니다.")
-        }
-
-        return user.id
-    }
+    ): Long = userRepository.findByEmail(request.email)?.run {
+        require(bCryptPasswordEncoder.matches(request.password, password)) { "비밀번호가 일치하지 않습니다." }
+        id
+    } ?: error("존재하지 않는 계정입니다.")
 
 }
