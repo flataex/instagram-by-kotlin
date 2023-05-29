7b9563eaafce7307fd7e5974a229bfc4d68e612d
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2023-05-29 16:41:51 +0900

    feat: 피드 저장, 조회 controller 추가

---
+++ shkim/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/feed/controller/FeedController.kt
@@ -0,0 +1,32 @@
+package com.flata.instagram.domain.feed.controller
+
+import com.flata.instagram.domain.feed.controller.dto.FeedResponse
+import com.flata.instagram.domain.feed.controller.dto.FeedSaveRequest
+import com.flata.instagram.domain.feed.service.FeedService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/feeds")
+class FeedController(
+    private val feedService: FeedService
+) {
+
+    @GetMapping
+    fun findAll(
+        @RequestHeader(value = "Authorization") userId: String,
+    ): ResponseEntity<List<FeedResponse>> {
+        val userId2 = userId.split(" ")[1].toLong()
+        return ResponseEntity(feedService.findAll(userId2), HttpStatus.OK)
+    }
+
+    @PostMapping
+    fun save(
+        @RequestHeader(value = "Authorization") userId: String,
+        @ModelAttribute request: FeedSaveRequest
+    ): ResponseEntity<Long> {
+        val userId2 = userId.split(" ")[1].toLong()
+        return ResponseEntity(feedService.save(userId2, request), HttpStatus.CREATED)
+    }
+}
