b4e6158e95cdae66b6a2136dd76a45f8b19ed00c
Donggeon Moon <nullsafer@naver.com>
2023-05-31 00:47:55 +0900

    :sparkles: feat: file 서비스 작성

---
+++ dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/file/service/FileService.kt
@@ -0,0 +1,77 @@
+package com.flata.instagram.domain.file.service
+
+import com.flata.instagram.domain.file.dto.FileRequest
+import com.flata.instagram.domain.file.dto.FileResponse
+import com.flata.instagram.domain.file.model.File
+import com.flata.instagram.domain.file.repository.FileRepository
+import com.flata.instagram.global.exception.NoDataException
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.multipart.MultipartFile
+import java.util.*
+import javax.servlet.http.HttpServletRequest
+
+@Service
+class FileService(
+    private val fileRepository: FileRepository,
+) {
+    @Transactional(readOnly = true)
+    fun getFiles(): List<FileResponse> {
+        val files = fileRepository.findAll()
+        return files.stream()
+            .map { file ->
+                FileResponse(
+                    file.id,
+                    file.url,
+                    file.feedId
+                )
+            }.toList()
+
+    }
+
+    @Transactional(readOnly = true)
+    fun getFile(id: Long): FileResponse {
+        val file = fileRepository.findById(id)
+            .orElseThrow {
+                throw NoDataException()
+            }
+        return FileResponse(
+            file.id,
+            file.url,
+            file.feedId
+        )
+    }
+    @Transactional
+    fun saveFile(fileRequest: FileRequest, file: MultipartFile, request: HttpServletRequest): Long {
+        val realPath = request.servletContext.getRealPath("/")
+        val fileName = UUID.randomUUID().toString()
+            .plus(".")
+            .plus(
+                file.originalFilename?.split(".")?.get(1)
+            )
+        val filePath = realPath.plus(
+            "/".plus(
+                fileName
+            )
+        )
+
+        file.transferTo(
+            java.io.File(filePath)
+        )
+
+        return fileRepository.save(
+            File(
+                0,
+                "/".plus(fileName),
+                fileRequest.feedId
+            )
+        ).id
+    }
+
+    @Transactional
+    fun deleteFile(fileRequest: FileRequest) {
+        val fileToUpdate = fileRepository.findById(fileRequest.id)
+            .orElseThrow { throw NoDataException() }
+        fileToUpdate.delete()
+    }
+}
