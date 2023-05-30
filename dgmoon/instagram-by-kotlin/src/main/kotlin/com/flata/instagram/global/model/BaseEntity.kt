1d8c1f439d4f6e97947228ef85a6f9739e72cace
Donggeon Moon <nullsafer@naver.com>
2023-05-31 00:49:48 +0900

    :sparkles: feat: base entity 작성

---
+++ dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/global/model/BaseEntity.kt
@@ -0,0 +1,26 @@
+package com.flata.instagram.global.model
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.EntityListeners
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity(
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+    @LastModifiedDate
+    @Column(name = "modified_at", nullable = false)
+    var modifiedAt: LocalDateTime = LocalDateTime.now(),
+    @Column(name = "deleted_at")
+    var deletedAt: LocalDateTime? = null
+) {
+    fun delete() {
+        this.deletedAt = LocalDateTime.now()
+    }
+}
