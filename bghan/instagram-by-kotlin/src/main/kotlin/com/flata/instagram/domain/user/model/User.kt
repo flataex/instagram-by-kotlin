27987d7b4d76ef67619cc61000b405b4d860c841
white-gyu <white_gyu@naver.com>
2023-05-30 22:46:43 +0900

    feat: user entity soft delete 및 비밀번호 converter 로직 구현

--- bghan/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/user/model/User.kt
+++ bghan/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/user/model/User.kt
@@ -1,10 +1,15 @@
 package com.flata.instagram.domain.user.model
 
+import com.flata.instagram.domain.user.converter.PasswordEncryptConverter
 import com.flata.instagram.global.model.BaseEntity
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.Where
 import javax.persistence.*
 
 @Entity
 @Table(name = "users")
+@SQLDelete(sql = "update user set deleted_at = now() where id = ?")
+@Where(clause = "deleted_at is null")
 class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,6 +19,7 @@ class User(
     @Column(name = "email", nullable = false, length = 32, unique = true)
     var email: String,
 
+    @Convert(converter = PasswordEncryptConverter::class)
     @Column(name = "password", nullable = false, length = 256)
     var password: String,
 
