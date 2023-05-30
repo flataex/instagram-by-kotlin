6b1b8af6e271719ea71bda5edb78cd798d162800
white-gyu <white_gyu@naver.com>
2023-05-30 22:49:19 +0900

    feat: security all open

---
+++ bghan/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/global/config/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package com.flata.instagram.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig: WebSecurityConfigurerAdapter() {
+
+    override fun configure(httpSecurity: HttpSecurity) {
+        httpSecurity.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/**").permitAll()
+    }
+
+    @Bean
+    fun bCryptPasswordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
+}
