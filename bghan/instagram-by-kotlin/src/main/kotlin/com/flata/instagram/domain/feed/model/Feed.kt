c63a4a30043a470480c042f1e98fd04202da6980
white-gyu <white_gyu@naver.com>
2023-05-30 22:46:28 +0900

    feat: entity soft delete 대응

--- bghan/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/feed/model/Feed.kt
+++ bghan/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/feed/model/Feed.kt
@@ -2,10 +2,14 @@ package com.flata.instagram.domain.feed.model
 
 import com.flata.instagram.domain.user.model.User
 import com.flata.instagram.global.model.BaseEntity
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.Where
 import javax.persistence.*
 
 @Entity
 @Table(name = "feed")
+@SQLDelete(sql = "update feed set deleted_at = now() where id = ?")
+@Where(clause = "deleted_at is null")
 class Feed(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- bghan/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/file/model/File.kt
+++ bghan/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/file/model/File.kt
@@ -2,10 +2,14 @@ package com.flata.instagram.domain.file.model
 
 import com.flata.instagram.domain.feed.model.Feed
 import com.flata.instagram.global.model.BaseEntity
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.Where
 import javax.persistence.*
 
 @Entity
 @Table(name = "file")
+@SQLDelete(sql = "update file set deleted_at = now() where id = ?")
+@Where(clause = "deleted_at is null")
 class File(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
