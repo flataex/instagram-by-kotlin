c26626d1b46f5d267a6d46922f72a42e963bddfa
Donggeon Moon <nullsafer@naver.com>
2023-05-31 00:50:07 +0900

    :sparkles: feat: feed 컨트롤러 작성

---
+++ dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/feed/controller/FeedController.kt
@@ -0,0 +1,35 @@
+package com.flata.instagram.domain.feed.controller
+
+import com.flata.instagram.domain.feed.dto.FeedRequest
+import com.flata.instagram.domain.feed.dto.FeedResponse
+import com.flata.instagram.domain.feed.service.FeedService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.URI
+
+@RequestMapping("/feeds")
+class FeedController(
+    private val feedService: FeedService
+) {
+    @GetMapping
+    fun getFeeds(): ResponseEntity<List<FeedResponse>> {
+        return ResponseEntity.ok(feedService.getFeeds())
+    }
+
+    @GetMapping("/{id}")
+    fun getFeed(@PathVariable id: Long): ResponseEntity<FeedResponse> {
+        return ResponseEntity.ok(feedService.getFeed(id))
+    }
+
+    @PostMapping
+    fun saveFeed(feedRequest: FeedRequest): ResponseEntity<Any> {
+        val savedFeedId = feedService.saveFeed(feedRequest)
+        return ResponseEntity.created(URI.create("/feeds/".plus(savedFeedId))).build()
+    }
+
+    @DeleteMapping
+    fun deleteFeed(feedRequest: FeedRequest): ResponseEntity<Any> {
+        feedService.deleteFeed(feedRequest)
+        return ResponseEntity.noContent().build()
+    }
+}
