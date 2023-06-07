8d41aee34576ca0449a25a1f03d7bc1f31b41a54
BaekGyu Han <white_gyu@naver.com>
2023-06-07 15:23:39 +0900

    feat: 회원가입, 로그인, 게시글 CRUD 기능 구현 (#13)
    * chore: user, feed, file table 쿼리 생성
    
    * chore: spring initialization 시 table 생성 후 jpa initialization 진행
    
    * feat: 각 entity 공통 필드 위한 추상 클래스 생성
    
    * feat: user entity 생성
    
    * feat: file entity 생성
    
    * feat: feed entity 생성
    
    * feat: record 생성/수정 시간 자동 할당 위한 @EnableJpaAuditing 활성화
    
    * chore: jpa query value logging & bean 중복 이슈 해결
    
    * chore: 비밀번호 암호화, bean validation 라이브러리 추가 및 jpa all open
    
    * feat: entity soft delete 대응
    
    * feat: user entity soft delete 및 비밀번호 converter 로직 구현
    
    * feat: application 예외 반환 객체 구현
    
    * feat: 예외 enum 구현
    
    * feat: logging 위한 string writer, printwriter bean 등록
    
    * feat: 비밀번호 encoder bean 등록
    
    * feat: 비밀번호 암호화 entity converter 구현
    
    * feat: security all open
    
    * feat: 회원가입, 로그인 request/response dto 생성
    
    * feat: user entity converter 구현
    
    * feat: user repository 생성 및 email/nickname 존재 여부 및 email로 entity 조회 로직 구현
    
    * feat: email, nickname unique validation 구현
    
    * feat: 로그인 시 이메일, 비밀번호 오류 예외 구현
    
    * feat: exception converter 구현
    
    * feat: 전역 exception handler 구현
    
    * feat: 회원가입 및 로그인 service logic 구현
    
    * feat: 회원가입 및 로그인 api 생성
    
    * chore: file 업로드 경로 gitignore 추가
    
    * chore: file 업로드 root 경로 추가
    
    * feat: authorization header parsing 및 check 로직 구현
    
    * feat: constraintViolationException 예외 처리 추가
    
    * feat: 사용자, 게시글, HTTP API 예외 처리 추가
    
    * feat: 게시글 작성자 여부 로직 구현
    
    * feat: 게시글, 사용자 API request/response 객체 구현
    
    * feat: user entity -> dto 변환 로직 구현
    
    * feat: feed entity -> dto 변환 로직 구현
    
    * feat: feed repository 및 사용자가 작성한 게시글 조회 기능 구현
    
    * feat: 파일명으로 파일 업로드 url 경로 생성 로직 구현
    
    * feat: file repository 생성 및 게시글 올릴 때 업로드한 파일 조회 기능 구현
    
    * feat: user id로 entity 조회 기능 구현
    
    * feat: 파일 업로드 및 url 저장 로직 구현
    
    * feat: feed crud 로직 구현
    
    * feat: feed crud facade 구현
    
    * feat: feed crud api 구현
    
    * chore: feed converter feed로 directory 이동
    
    * refactor: 로그인 확인 메서드명 변경
    
    * refactor: stream toList 개선

---
+++ bghan/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/user/model/User.kt
@@ -0,0 +1,30 @@
+package com.flata.instagram.domain.user.model
+
+import com.flata.instagram.domain.user.converter.PasswordEncryptConverter
+import com.flata.instagram.global.model.BaseEntity
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.Where
+import javax.persistence.*
+
+@Entity
+@Table(name = "users")
+@SQLDelete(sql = "update user set deleted_at = now() where id = ?")
+@Where(clause = "deleted_at is null")
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long = 0,
+
+    @Column(name = "email", nullable = false, length = 32, unique = true)
+    var email: String,
+
+    @Convert(converter = PasswordEncryptConverter::class)
+    @Column(name = "password", nullable = false, length = 256)
+    var password: String,
+
+    @Column(name = "nickname", nullable = false, length = 16, unique = true)
+    var nickname: String
+): BaseEntity() {
+    fun isSame(userId: Long): Boolean = this.id.compareTo(userId) == 0
+}
