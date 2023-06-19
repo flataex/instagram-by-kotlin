bf3c518508d37d7601b43671e2b6e12cf929cff4
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2023-06-19 21:37:48 +0900

    feat: 팔로우, 팔로우 취소 service 로직 추가

---
+++ shkim/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/follow/service/FollowService.kt
@@ -0,0 +1,37 @@
+package com.flata.instagram.domain.follow.service
+
+import com.flata.instagram.domain.follow.model.Follow
+import com.flata.instagram.domain.follow.repository.FollowRepository
+import com.flata.instagram.domain.user.repository.UserRepository
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class FollowService(
+    private val userRepository: UserRepository,
+    private val followRepository: FollowRepository
+) {
+
+    fun save(
+        userId: Long,
+        toUserId: Long
+    ): Long {
+        return Follow(
+                fromUser = userRepository.findByIdOrNull(userId) ?: throw NotFoundException(),
+                toUser = userRepository.findByIdOrNull(toUserId) ?: throw NotFoundException(),
+            ).let { followRepository.save(it) }
+            .id
+    }
+
+    fun delete(
+        userId: Long,
+        toUserId: Long
+    ) {
+        followRepository.findByFromUserIdAndToUserId(userId, toUserId)
+            ?.delete()
+            ?: throw NotFoundException()
+    }
+}
