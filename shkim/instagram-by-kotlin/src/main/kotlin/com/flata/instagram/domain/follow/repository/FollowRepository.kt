bbf7fb0151d568d5ef772c1e8b10a0fe18e93109
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2023-06-19 21:37:29 +0900

    feat: findByFromUserIdAndToUserId 쿼리 메서드 추가

---
+++ shkim/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/follow/repository/FollowRepository.kt
@@ -0,0 +1,8 @@
+package com.flata.instagram.domain.follow.repository
+
+import com.flata.instagram.domain.follow.model.Follow
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface FollowRepository: JpaRepository<Follow, Long> {
+    fun findByFromUserIdAndToUserId(fromUserId: Long, toUserId: Long): Follow?
+}
