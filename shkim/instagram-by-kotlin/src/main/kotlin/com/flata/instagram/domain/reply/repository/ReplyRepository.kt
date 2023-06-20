495aeec1d0779e2c89a91bb0c9cb8442ec2347da
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2023-06-20 21:26:31 +0900

    feat: ReplyRepository에 findByCommentId 쿼리 메서드 추가

--- shkim/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/reply/repository/ReplyRepository.kt
+++ shkim/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/reply/repository/ReplyRepository.kt
@@ -4,4 +4,5 @@ import com.flata.instagram.domain.reply.model.Reply
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ReplyRepository: JpaRepository<Reply, Long> {
+    fun findByCommentId(commentId: Long): List<Reply>
 }
