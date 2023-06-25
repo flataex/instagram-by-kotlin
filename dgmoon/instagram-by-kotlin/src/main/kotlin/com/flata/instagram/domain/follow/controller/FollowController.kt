dd2b335ad248b99106aadcf72647dea7057efe68
Donggeon Moon <nullsafer@naver.com>
2023-06-26 00:47:55 +0900

    fix: URL, 메서드 수정

--- dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/follow/controller/FollowController.kt
+++ dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/follow/controller/FollowController.kt
@@ -1,38 +1,28 @@
 package com.flata.instagram.domain.follow.controller
 
 import com.flata.instagram.domain.follow.dto.FollowRequest
-import com.flata.instagram.domain.follow.dto.FollowResponse
 import com.flata.instagram.domain.follow.service.FollowService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpSession
 import javax.validation.Valid
 
 @RestController
+@RequestMapping("/follows")
 class FollowController(
     private val followService: FollowService
 ) {
-    @PostMapping("/follow")
+    @PostMapping
     fun follow(
         @Valid @RequestBody followRequest: FollowRequest,
         httpSession: HttpSession
-    ): ResponseEntity<FollowResponse> =
-        ResponseEntity.ok(
-            FollowResponse(
-                followService.follow(followRequest, httpSession.getAttribute("userId") as Long)
-            )
-        )
+    ): ResponseEntity<Unit> =
+        followService.follow(followRequest, httpSession.getAttribute("userId") as Long)
 
-    @PostMapping("/unfollow")
+    @DeleteMapping
     fun unfollow(
         @Valid @RequestBody followRequest: FollowRequest,
         httpSession: HttpSession
-    ): ResponseEntity<FollowResponse> =
-        ResponseEntity.ok(
-            FollowResponse(
-                followService.unfollow(followRequest, httpSession.getAttribute("userId") as Long)
-            )
-        )
+    ): ResponseEntity<Unit> =
+        followService.unfollow(followRequest, httpSession.getAttribute("userId") as Long)
 }
--- dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/like/controller/LikeController.kt
+++ dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/like/controller/LikeController.kt
@@ -4,30 +4,23 @@ import com.flata.instagram.domain.like.dto.LikeRequest
 import com.flata.instagram.domain.like.dto.LikeResponse
 import com.flata.instagram.domain.like.service.LikeService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpSession
 import javax.validation.Valid
 
-@RestController
+@RestController("/likes")
 class LikeController(
     private val likeService: LikeService
 ) {
-    @PostMapping("/like")
-    fun like(@Valid @RequestBody likeRequest: LikeRequest, httpSession: HttpSession): ResponseEntity<LikeResponse> =
-        ResponseEntity.ok(
-            LikeResponse(
-                likeService.like(likeRequest, httpSession.getAttribute("userId") as Long)
-            )
-        )
+    @PostMapping
+    fun like(@Valid @RequestBody likeRequest: LikeRequest, httpSession: HttpSession): ResponseEntity<Unit> =
+        likeService.like(likeRequest, httpSession.getAttribute("userId") as Long)
 
 
-    @PostMapping("/unlike")
-    fun unlike(@Valid @RequestBody likeRequest: LikeRequest, httpSession: HttpSession): ResponseEntity<LikeResponse> =
-        ResponseEntity.ok(
-            LikeResponse(
-                likeService.unlike(likeRequest, httpSession.getAttribute("userId") as Long)
-            )
-        )
+    @DeleteMapping
+    fun unlike(@Valid @RequestBody likeRequest: LikeRequest, httpSession: HttpSession): ResponseEntity<Unit> =
+        likeService.unlike(likeRequest, httpSession.getAttribute("userId") as Long)
 }
