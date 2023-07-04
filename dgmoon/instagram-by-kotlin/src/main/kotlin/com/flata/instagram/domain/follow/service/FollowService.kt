add43e130d74f7a73e979faae98ce75f5b2728e7
Donggeon Moon <nullsafer@naver.com>
2023-07-05 02:18:22 +0900

    fix: 예외 처리 runcatching으로 변경 중

--- dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/follow/service/FollowService.kt
+++ dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/follow/service/FollowService.kt
@@ -1,6 +1,7 @@
 package com.flata.instagram.domain.follow.service
 
 import com.flata.instagram.domain.follow.dto.FollowRequest
+import com.flata.instagram.domain.follow.dto.FollowResponse
 import com.flata.instagram.domain.follow.model.Follow
 import com.flata.instagram.domain.follow.repository.FollowRepository
 import com.flata.instagram.domain.user.repository.UserRepository
@@ -21,52 +22,64 @@ class FollowService(
 
 ) {
     @Transactional
-    fun follow(followRequest: FollowRequest, userId: Long): ResponseEntity<Unit> {
-        val zSetOperations = redisTemplate.opsForZSet()
-        zSetOperations.operations.multi()
+    fun follow(followRequest: FollowRequest, userId: Long): ResponseEntity<FollowResponse> =
+        runCatching {
+            val zSetOperations = redisTemplate.opsForZSet()
+            zSetOperations.operations.multi()
 
-        zSetOperations.add(
-            "following:".plus(userId),
-            followRequest.toUserId.toString(),
-            Timestamp.valueOf(LocalDateTime.now()).time.toDouble()
-        )
-
-        zSetOperations.add(
-            "followers:".plus(followRequest.toUserId),
-            userId.toString(),
-            Timestamp.valueOf(LocalDateTime.now()).time.toDouble()
-        )
-
-        followRepository.save(
-            Follow(
-                0L,
-                userRepository.findByIdOrNull(userId) ?: throw NoDataException(),
-                userRepository.findByIdOrNull(followRequest.toUserId) ?: throw NoDataException()
+            zSetOperations.add(
+                "following:".plus(userId),
+                followRequest.toUserId.toString(),
+                Timestamp.valueOf(LocalDateTime.now()).time.toDouble()
             )
-        )
 
-        return ResponseEntity.ok().build()
-    }
+            zSetOperations.add(
+                "followers:".plus(followRequest.toUserId),
+                userId.toString(),
+                Timestamp.valueOf(LocalDateTime.now()).time.toDouble()
+            )
+
+                followRepository.save(
+                    Follow(
+                        0L,
+                        userRepository.findByIdOrNull(userId) ?: throw NoDataException(),
+                        userRepository.findByIdOrNull(followRequest.toUserId) ?: throw NoDataException()
+                    )
+                ).let {
+                    ResponseEntity.ok(
+                        FollowResponse(
+                            true
+                        )
+                    )
+                }
+        }.onFailure {
+            throw Exception()
+        }.getOrThrow()
 
     @Transactional
-    fun unfollow(followRequest: FollowRequest, userId: Long): ResponseEntity<Unit> {
-        val zSetOperations = redisTemplate.opsForZSet()
-        zSetOperations.operations.multi()
+    fun unfollow(followRequest: FollowRequest, userId: Long): ResponseEntity<Unit> =
+        runCatching {
+            val zSetOperations = redisTemplate.opsForZSet()
+            zSetOperations.operations.multi()
 
-        zSetOperations.remove(
-            "following:".plus(userId),
-            followRequest.toUserId.toString(),
-        )
+            zSetOperations.remove(
+                "following:".plus(userId),
+                followRequest.toUserId.toString(),
+            )
 
-        zSetOperations.remove(
-            "followers:".plus(followRequest.toUserId),
-            userId.toString(),
-        )
+            zSetOperations.remove(
+                "followers:".plus(followRequest.toUserId),
+                userId.toString(),
+            )
 
-        val follow = followRepository.findByIdOrNull(followRequest.id) ?: throw NoDataException()
+            followRepository.findByIdOrNull(followRequest.id)
+                ?.let {
+                    it.delete()
+                    ResponseEntity.noContent().build<Unit>()
+                }
+                ?: throw NoDataException()
+        }.onFailure {
+            throw Exception()
+        }.getOrThrow()
 
-        follow.delete()
-
-        return ResponseEntity.ok().build()
-    }
 }
