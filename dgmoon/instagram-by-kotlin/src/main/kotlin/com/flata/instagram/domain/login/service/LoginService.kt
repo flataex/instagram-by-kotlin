bc9bb0f16066ebafa83deff5856964df994f113f
Donggeon Moon <nullsafer@naver.com>
2023-07-09 17:02:35 +0900

    refactor: BcryptPasswordEncoder 빈 등록

--- dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/login/service/LoginService.kt
+++ dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/domain/login/service/LoginService.kt
@@ -3,20 +3,21 @@ package com.flata.instagram.domain.login.service
 import com.flata.instagram.domain.login.dto.LoginRequest
 import com.flata.instagram.domain.user.repository.UserRepository
 import com.flata.instagram.global.exception.InvalidLoginInfoException
-import org.springframework.security.crypto.bcrypt.BCrypt
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import javax.servlet.http.HttpSession
 
 @Service
 class LoginService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val encoder: BCryptPasswordEncoder
 ) {
     fun login(loginRequest: LoginRequest, session: HttpSession): Long =
         userRepository.findByEmail(loginRequest.email)
             ?.let {
                 session.setAttribute("userId", it.id)
                 it.id.takeIf {
-                    BCrypt.checkpw(
+                    encoder.matches(
                         userRepository.findByEmail(loginRequest.email)!!
                             .password,
                         loginRequest.password
