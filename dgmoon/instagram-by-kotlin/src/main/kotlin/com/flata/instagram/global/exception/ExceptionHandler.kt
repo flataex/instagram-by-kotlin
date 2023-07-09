b9e8b333b4bc46ebdf956773a181ced8c79699ae
Donggeon Moon <nullsafer@naver.com>
2023-07-09 17:21:42 +0900

    refactor: unique 제약 조건 위반 시 409 Conflict 상태코드를 반환하도록 수정

--- dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/global/exception/ExceptionHandler.kt
+++ dgmoon/instagram-by-kotlin/src/main/kotlin/com/flata/instagram/global/exception/ExceptionHandler.kt
@@ -3,7 +3,6 @@ package com.flata.instagram.global.exception
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
@@ -14,5 +13,5 @@ class ExceptionHandler {
 
     @ExceptionHandler(NotUniqueColumnException::class)
     fun handleNotUniqueColumnException(): ResponseEntity<Any> =
-        ResponseEntity.badRequest().build()
+        ResponseEntity(HttpStatus.CONFLICT)
 }
